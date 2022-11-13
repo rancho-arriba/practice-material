62d4c4a0b902cf7bb30c1977519b8717e4fdd470
Curtis Roy <curtisroy@MacBook-Pro.local>
2022-11-13 10:01:11 -0800

    Add work on latest kata

---
+++ read_write_file.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3
+
+""" Reading from and writing/appending to a file."""
+
+def write_to_file(filename, text):
+    """ Write text to the file"""
+    # stuff
+
+write_to_file(greet.txt, 'Hello!\n')
+
+def append_to_file(filename, text):
+    """ Append text to the file"""
+    # stuff
+
+append_to_file(greet.txt, 'Goodbye!\n')
+
+def read_from_file(filename):
+    """ Read text from the file"""
+    # stuff
+
+read_from_file(greet.txt)
