fe925b73dffbe651356ba0985b47987cd140cf56
rancho-arriba <curtisroy@MacBook-Pro.local>
2023-02-05 08:10:19 -0800

    Directory restructure

---
+++ python/12_smallest_biggest.py
@@ -0,0 +1,7 @@
+""" Find the smallest or biggest number in a list of numbers
+    without using min()."""
+
+def get_smallest(numbers):
+    """ Find the smallest number."""
+    if len(get_smallest) == 0:
+        return None
